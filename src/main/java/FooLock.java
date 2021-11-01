7b2da913e5c157056ac6528e8bac27d85feaaa55
Anton <anton.tseniukh@gmail.com>
2021-11-01 18:13:06 +0200

    add condition

--- src/main/java/FooLock.java
+++ src/main/java/FooLock.java
@@ -1,37 +1,51 @@
 import java.util.ConcurrentModificationException;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 
 public class FooLock {
-    private final Lock lock = new ReentrantLock();
+    private final Lock lock = new ReentrantLock(true);
     private final Condition cond = lock.newCondition();
 
     public void first(Runnable r) throws InterruptedException {
+        lock.lock();
         try {
-            lock.lock();
-            //cond.await();
+            while (Thread.currentThread().isInterrupted())
+                cond.await(1, TimeUnit.SECONDS);
+            cond.signal();
             System.out.print("first");
+        } catch (InterruptedException e){
+            e.printStackTrace();
         } finally {
             lock.unlock();
         }
     }
 
     public void second(Runnable r) {
+        lock.lock();
         try {
-            lock.lock();
+            while (Thread.currentThread().isInterrupted())
+                cond.await(10, TimeUnit.SECONDS);
+            cond.signal();
             System.out.print("second");
-            cond.signalAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         } finally {
             lock.unlock();
         }
     }
 
     public void third(Runnable r) {
+        lock.lock();
         try {
-            lock.lock();
+            while (Thread.currentThread().isInterrupted())
+                cond.await(15, TimeUnit.SECONDS);
+            cond.signal();
             System.out.print("third");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         } finally {
             lock.unlock();
         }
